93638ae3ce47410c23b5c3bd141b44a0f38e4a42
sowntharya <ssowntharya33@gmail.com>
2016-06-06 14:08:24 +0530

    Create ispostive.c

---
+++ ispostive.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+    printf("Zero");
+    else if(n<0)
+    printf("Negative");
+    else
+    printf("Positive");
+    return 0;
+}
